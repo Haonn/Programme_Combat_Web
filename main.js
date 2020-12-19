b8a245532aecdbd39c45d27d0222d84e7d6e7cc0
Haonn <74655091+Haonn@users.noreply.github.com>
2020-12-19 16:52:53 +0100

    Création des fichiers, premières tetnatives

--- main.js
+++ main.js
@@ -1,48 +1,16 @@
-alert("préparez-vous au combat !");
-let combatGagne = false;
-let combatPerdu = false ;
-let p1 = [15,false];
+var pvKaezh = document.getElementById("pvp1")
+var atk = 6
+var pvM1 = 20
+var choixAttaque = document.getElementById("attaquer")
 
-var pvMonstre = document.getElementById("pvMonstre");
+var combatEnCours = 1
 
-var attaque = document.getElementById('attaquer');
-var defense = document.getElementById('defendre');
-var soin = document.getElementById('soigner');
-function attaque (){
-	pvMontre.innerHTML = pvMonstre.innerHTML - 6;
-}
-function defense (){
-	p1[2] = true;
-}
-function soin (){
-	p1[1]=+ 4;
+function attaque(pvM1, atk){
+	pvM1 = pvM1 - atk
 }
 
-while ("combatGagne"!== true & "combatPerdu "!== true ){
-	
-	attaque.onclick = function(){attaque(p1);}
-	
-	defense.onclick = function(){defense(p1);}
-
-	soin.onclick = function(){soin(p1);}
-
-	if (pvMonstre = 0){
-		combatGagne = true;
-	}
-	if (p1[2] = false ){
-		p1[1] = p1[1]-5;
-	}
-	if (p1[1] = 0){
-		combatPerdu = true;
-	}
-	if (p1[1] < 0){
-		combatPerdu = true;
-	}
-
+function update (pvKaezh){
+	pvp1.innerHTML = pvKaezh
 }
-if (combatPerdu = true){
-	alert("Vous avez perdu");
-}
-if (combatGagne = true){
-	alert("vous avez gagné !");
-}
+choixAttaque.onclick = function(){attaque(pvM1, atk)}
+
