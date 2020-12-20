7048d91d44ceff252e702f6442ae333f6cf40a9d
Haonn <74655091+Haonn@users.noreply.github.com>
2020-12-20 22:00:48 +0100

    1v1 attaque infobulle victoire/défaite

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-var pvKaezh = document.getElementById("pvp1")
-var atk = 6
-var pvM1 = 20
-var choixAttaque = document.getElementById("attaquer")
+var pvKaezh = document.getElementById("pvp1");
+var atkKaezh = 6;
+var soinKaezh = 7;
+var pvMonstre1 = document.getElementById("viem1");
+var choixAttaque = document.getElementById("attaquer");
+var choixSoin = document.getElementById("soigner");
+var monstre1 = document.getElementById("imgMonstre");
 
-var combatEnCours = 1
-
-function attaque(pvM1, atk){
-	pvM1 = pvM1 - atk
+function attaque(pvMonstre, scoreAttaque, pvJoueur){
+	pvMonstre.innerHTML = pvMonstre.innerHTML - scoreAttaque
+	if (pvMonstre.innerHTML < 1) {
+		alert ("Vous avez gagné ! ")
+		document.getElementById("imgMonstre").style.visibility = "hidden"
+	} else {
+		pvJoueur.innerHTML = pvJoueur.innerHTML - 5
+		if (pvJoueur.innerHTML < 1){
+			alert ("Vous êtes mort")
+		}
+	}
+	
 }
+function soin(pvJoueur){
+	pvJoueur.innerHTML = pvJoueur.innerHTML + 7
 
-function update (pvKaezh){
-	pvp1.innerHTML = pvKaezh
 }
-choixAttaque.onclick = function(){attaque(pvM1, atk)}
+monstre1.addEventListener("mouseover", () => {pvMonstre1.style.visibility = "visible";})
+monstre1.addEventListener("mouseout", () => {pvMonstre1.style.visibility = "hidden";})
 
+choixAttaque.onclick = function(){attaque(pvMonstre1, atkKaezh, pvKaezh)}
+choixSoin.onclick = function(){soin(pvKaezh)}
